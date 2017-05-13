d13a6e3e88d17e6d5c4b6dab12b50194e563271d
Victor <Victor@DESKTOP-MA4MGSE>
2017-05-13 17:50:42 +0900

    유해싸이트 수정

--- administrator_program/src/com/kitri/admin/main/control/ControlJP.java
+++ administrator_program/src/com/kitri/admin/main/control/ControlJP.java
@@ -12,7 +12,7 @@ public class ControlJP extends JPanel {
 	public ControlJP() {
 		setLayout(new GridLayout(10, 1, 0, 0));
 		
-		JButton btnNewButton_1 = new JButton("\uC720\uD574\uD504\uB85C\uADF8\uB7A8 \uCC28\uB2E8");
+		JButton btnNewButton_1 = new JButton("\uC720\uD574\uC2F8\uC774\uD2B8\uCC28\uB2E8 \uAD00\uB9AC");
 		add(btnNewButton_1);
 		
 		JButton btnNewButton = new JButton("\uB098\uC774\uC81C\uD55C");
---
+++ administrator_program/src/com/kitri/admin/main/control/ListOfBlockedSites.java
@@ -0,0 +1,42 @@
+package com.kitri.admin.main.control;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ListOfBlockedSites extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListOfBlockedSites frame = new ListOfBlockedSites();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListOfBlockedSites() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
