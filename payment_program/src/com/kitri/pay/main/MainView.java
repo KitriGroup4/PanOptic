b75901990d58c6fc70b2afc9695a534c251e100b
ckdtj <ckdtj@CS_NOTE_2>
2017-05-13 15:48:24 +0900

    dddddd

--- payment_program/src/com/kitri/pay/main/MainView.java
+++ payment_program/src/com/kitri/pay/main/MainView.java
@@ -12,35 +12,31 @@ public class MainView extends JFrame {
 
     private final int TIME_COUNT = 8;
     private final int POINT_COUNT = 5;
-    
-    private String[] timeStr = {"1시간", "1시간 30분", "2시간", "2시간30분", "3시간", "4시간", "5시간", "7시간"};
-    private String[] pointStr = {"1000P", "3000P", "5000P", "10000P", "15000P"}; 
-    
+    private final int FUNCTION_COUNT = 3;
+
+    private String[] timeStr = { "1시간", "1시간 30분", "2시간", "2시간30분", "3시간", "4시간", "5시간", "7시간" };
+    private String[] pointStr = { "1000P", "3000P", "5000P", "10000P", "15000P" };
+    private String[] functionStr = { "회원가입", "포인트결제", "카드결제" };
+
     private JButton[] timeButton;
     private JButton[] pointButton;
-    
+    private JButton[] functionButton;
+
     private JPanel contentPane;
-    private JButton btnNewButton;
-    private JButton btnNewButton_1;
-    private JButton btnNewButton_2;
-    private JButton btnNewButton_3;
-    private JButton btnNewButton_4;
-    private JButton btnNewButton_5;
-    private JButton btnNewButton_6;
-    private JButton btnNewButton_7;
-    private JButton btnNewButton_8;
-    private JButton btnNewButton_9;
-    private JButton btnNewButton_10;
-    private JButton btnNewButton_11;
-    private JButton btnNewButton_12;
-    private JButton btnNewButton_13;
-    private JButton btnNewButton_14;
-    private JPanel timePanel;
-    private JPanel pointPanel;
-    private JPanel panel_3;
+    private JPanel timeButtonPanel;
+    private JPanel pointButtonPanel;
+    private JPanel functionPanel;
     private JPanel mainPanel;
-    private JPanel panel;
+    private JPanel mainLabelPanel;
     private JLabel lblNewLabel;
+    private JPanel timePanel;
+    private JPanel timeLabelPanel;
+    private JLabel timeLabel;
+    private JPanel pointPanel;
+    private JPanel pointLabelPanel;
+    private JLabel pointLabel;
+    
+    private MainViewListener listener;
 
     /**
      * Launch the application.
@@ -62,91 +58,91 @@ public class MainView extends JFrame {
      * Create the frame.
      */
     public MainView() {
+	
+	listener = new MainViewListener(this);
+
 	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	setBounds(100, 100, 450, 300);
 	contentPane = new JPanel();
 	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 	setContentPane(contentPane);
 	contentPane.setLayout(new BorderLayout(0, 0));
-	
+
 	mainPanel = new JPanel();
 	contentPane.add(mainPanel, BorderLayout.CENTER);
 	mainPanel.setLayout(new GridLayout(1, 3, 40, 0));
-	
+
 	timePanel = new JPanel();
 	mainPanel.add(timePanel);
-	timePanel.setLayout(new GridLayout(4, 2, 20, 20));
-	
-	btnNewButton = new JButton("New button");
-	timePanel.add(btnNewButton);
-	
-	btnNewButton_1 = new JButton("New button");
-	timePanel.add(btnNewButton_1);
-	
-	btnNewButton_2 = new JButton("New button");
-	timePanel.add(btnNewButton_2);
-	
-	btnNewButton_3 = new JButton("New button");
-	timePanel.add(btnNewButton_3);
-	
-	btnNewButton_4 = new JButton("New button");
-	timePanel.add(btnNewButton_4);
-	
-	btnNewButton_5 = new JButton("New button");
-	timePanel.add(btnNewButton_5);
-	
-	btnNewButton_6 = new JButton("New button");
-	timePanel.add(btnNewButton_6);
-	
-	btnNewButton_7 = new JButton("New button");
-	timePanel.add(btnNewButton_7);
-	
+	timePanel.setLayout(new BorderLayout(0, 0));
+
+	timeButtonPanel = new JPanel();
+	timePanel.add(timeButtonPanel, BorderLayout.CENTER);
+	timeButtonPanel.setLayout(new GridLayout(4, 2, 20, 20));
+
+	timeButton = new JButton[TIME_COUNT];
+	for (int i = 0; i < TIME_COUNT; i++) {
+	    timeButton[i] = new JButton(timeStr[i]);
+	    timeButtonPanel.add(timeButton[i]);
+	}
+
+	timeLabelPanel = new JPanel();
+	timePanel.add(timeLabelPanel, BorderLayout.NORTH);
+
+	timeLabel = new JLabel("New label");
+	timeLabel.setFont(new Font("굴림", Font.PLAIN, 30));
+	timeLabelPanel.add(timeLabel);
+
 	pointPanel = new JPanel();
 	mainPanel.add(pointPanel);
-	pointPanel.setLayout(new GridLayout(4, 2, 20, 20));
-	
-	btnNewButton_8 = new JButton("New button");
-	pointPanel.add(btnNewButton_8);
-	
-	btnNewButton_9 = new JButton("New button");
-	pointPanel.add(btnNewButton_9);
-	
-	btnNewButton_10 = new JButton("New button");
-	pointPanel.add(btnNewButton_10);
-	
-	btnNewButton_11 = new JButton("New button");
-	pointPanel.add(btnNewButton_11);
-	
-	panel_3 = new JPanel();
-	mainPanel.add(panel_3);
-	panel_3.setLayout(new GridLayout(3, 1, 20, 20));
-	
-	btnNewButton_12 = new JButton("New button");
-	panel_3.add(btnNewButton_12);
-	
-	btnNewButton_13 = new JButton("New button");
-	panel_3.add(btnNewButton_13);
-	
-	btnNewButton_14 = new JButton("New button");
-	panel_3.add(btnNewButton_14);
-	
-	panel = new JPanel();
-	panel.setMinimumSize(new Dimension(10, 100));
-	contentPane.add(panel, BorderLayout.NORTH);
-	
+	pointPanel.setLayout(new BorderLayout(0, 0));
+
+	pointButtonPanel = new JPanel();
+	pointPanel.add(pointButtonPanel);
+	pointButtonPanel.setLayout(new GridLayout(4, 2, 20, 20));
+
+	pointButton = new JButton[POINT_COUNT];
+	for (int i = 0; i < POINT_COUNT; i++) {
+	    pointButton[i] = new JButton(pointStr[i]);
+	    pointButtonPanel.add(pointButton[i]);
+	}
+
+	pointLabelPanel = new JPanel();
+	pointPanel.add(pointLabelPanel, BorderLayout.NORTH);
+
+	pointLabel = new JLabel("New label");
+	pointLabel.setFont(new Font("굴림", Font.PLAIN, 30));
+	pointLabelPanel.add(pointLabel);
+
+	functionPanel = new JPanel();
+	mainPanel.add(functionPanel);
+	functionPanel.setLayout(new GridLayout(3, 1, 20, 20));
+
+	functionButton = new JButton[FUNCTION_COUNT];
+	for (int i = 0; i < FUNCTION_COUNT; i++) {
+	    functionButton[i] = new JButton(functionStr[i]);
+	    functionPanel.add(functionButton[i]);
+	}
+
+	mainLabelPanel = new JPanel();
+	mainLabelPanel.setMinimumSize(new Dimension(10, 100));
+	contentPane.add(mainLabelPanel, BorderLayout.NORTH);
+
 	lblNewLabel = new JLabel("New label");
-	panel.add(lblNewLabel);
+	lblNewLabel.setFont(new Font("굴림", Font.PLAIN, 60));
+	mainLabelPanel.add(lblNewLabel);
 
 	GraphicsEnvironment environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	GraphicsDevice device = environment.getDefaultScreenDevice();
 	this.setUndecorated(true);
 	device.setFullScreenWindow(this);
 
-//	if (device.isDisplayChangeSupported()) {
-//	    // refreshRate값은 따로 찾아 봐야함
-//	    DisplayMode displayMode = new DisplayMode(1024, 768, 32, DisplayMode.REFRESH_RATE_UNKNOWN);
-//	    device.setDisplayMode(displayMode);
-//	}
+	// if (device.isDisplayChangeSupported()) {
+	// // refreshRate값은 따로 찾아 봐야함
+	// DisplayMode displayMode = new DisplayMode(1024, 768, 32,
+	// DisplayMode.REFRESH_RATE_UNKNOWN);
+	// device.setDisplayMode(displayMode);
+	// }
     }
 
 }
---
+++ payment_program/src/com/kitri/pay/main/MainViewListener.java
@@ -0,0 +1,23 @@
+package com.kitri.pay.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainViewListener implements ActionListener {
+
+    private MainView view;
+    
+    public MainViewListener(MainView view) {
+	this.view = view;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent event) {
+	Object o = event.getSource();
+	
+	
+	
+	
+    }
+
+}
