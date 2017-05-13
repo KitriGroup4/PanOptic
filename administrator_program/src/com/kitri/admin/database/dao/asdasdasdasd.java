e123fa6819ea448640540137cb6cf681c0a4bd93
ckdtj <ckdtj@CS_NOTE_2>
2017-05-13 14:54:17 +0900

    delete class

--- administrator_program/src/com/kitri/admin/database/dao/asdasdasdasd.java
+++
@@ -1,8 +0,0 @@
-package com.kitri.admin.database.dao;
-
-public class asdasdasdasd {
-	public static void main(String[] args) {
-		System.out.println("123");
-	}
-
-}
--- administrator_program/src/com/kitri/admin/database/dao/fdsfsdfsdfsdfsd.java
+++
@@ -1,8 +0,0 @@
-package com.kitri.admin.database.dao;
-
-public class fdsfsdfsdfsdfsd {
-	public static void main(String[] args) {
-		System.out.println("¾ß¿ë");
-	}
-
-}
